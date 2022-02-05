65bdea5facc7468ad21b5e231f86620afd48c2a5
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-05 15:00:49 +0800

    Delete server directory

--- server/util/renderTemplate.js
+++
@@ -1,3 +0,0 @@
-export default function(res, title, page, data = {}) {
-	res.render("template", { title, page, data });
-}
--- server/util/reportError.js
+++
@@ -1,7 +0,0 @@
-import chalk from "chalk";
-
-export default function reportError(err, req) {
-	const prefix = req ? `Error at ${req.originalUrl}:` : `Error during request:`;
-	console.error(err);
-	console.error(chalk.red(`${prefix}, see above for more info`));
-}
