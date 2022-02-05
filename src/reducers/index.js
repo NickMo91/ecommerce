e893d0a2908fdac4b2068e940315a8cf9e7eb94d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-05 15:01:11 +0800

    Delete src directory

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import products from "./products";
-import cart from "./cart";
-import checkout from "./checkout";
-
-
-
-export default combineReducers({ products, cart, checkout });
